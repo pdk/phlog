f9b258aaf717c8d27a0d5743cf2ef656219833ab
Patrick D. Kelly <pdk@users.noreply.github.com>
2022-07-05 21:30:49 -1000

    flags for addr, port

--- phlog.go
+++ phlog.go
@@ -7,19 +7,23 @@ import (
 )
 
 type ServerConfig struct {
-	RootPath string
+	RootPath   string
+	ListenAddr string
+	ListenPort string
 }
 
 func main() {
 
 	conf := ServerConfig{}
 
+	flag.StringVar(&conf.ListenAddr, "listen", "", "ip address to listen for new connections")
+	flag.StringVar(&conf.ListenPort, "port", "8888", "port to listen for new connections")
 	flag.StringVar(&conf.RootPath, "root", "./docs", "path to directory containing files to serve")
 	flag.Parse()
 
-	log.Printf("starting server")
+	log.Printf("starting server at %s:%s", conf.ListenAddr, conf.ListenPort)
 
 	root := http.FileServer(http.Dir(conf.RootPath))
 
-	log.Fatal(http.ListenAndServe(":8888", root))
+	log.Fatal(http.ListenAndServe(conf.ListenAddr+":"+conf.ListenPort, root))
 }
